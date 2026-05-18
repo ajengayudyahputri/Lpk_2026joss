3ae558c5fc370ac1e5a83026efba3404c54e17a6
ajengayudyahputri <ajengayudyahputri@gmail.com>
2026-05-18 11:31:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='Ajeng.ayu',
+    page_title='Ajeng_ayu',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
